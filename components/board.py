efcfbf6655baf5c0f3a1edb8479f4ad2a5a9b621
Jakob Kirby <95430949+thatguy988@users.noreply.github.com>
2023-05-17 15:01:24 -0500

    Update board.py

--- components/board.py
+++ components/board.py
@@ -1,25 +1,24 @@
 import pygame
 from components.Pieces import Piece
-# Define colors
 LIGHT_GRAY = (180, 180, 180)
 DARK_GRAY = (100, 100, 100)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 class Board:
-    def __init__(self,rows,cols,square_size):
+    def __init__(self,rows,cols,square_size): #assign board object with attributes for rows, cols, squaresize
         self.rows=rows
         self.cols=cols
         self.square_size=square_size
-        self.grid = [[None] * cols for _ in range(rows)]
+        self.grid = [[None] * cols for _ in range(rows)]#2d list
 
-    def create_pieces(self):
-        for row in range(3):
+    def create_pieces(self):#populates board with checkers pieces
+        for row in range(3):# places black pieces on board iterates 0,1,2
             for col in range(self.cols):
                 if (row + col) % 2 == 0:
                     self.grid[row][col] = Piece(row, col, BLACK, self.square_size)
 
-        for row in range(5, self.rows):
+        for row in range(5, self.rows):#places white on board iterates from 5 to 7
             for col in range(self.cols):
                 if (row + col) % 2 == 0:
                     self.grid[row][col] = Piece(row, col, WHITE,self.square_size)
@@ -32,3 +31,17 @@ class Board:
                 piece = self.grid[row][col]
                 if piece:
                     piece.draw(screen)
+
+
+    def move_piece(self, piece, new_row, new_col):
+        current_row, current_col = piece.row, piece.col
+        if self.is_valid_move(current_row, current_col, new_row, new_col):
+            self.grid[current_row][current_col] = None
+            self.grid[new_row][new_col] = piece
+            piece.row = new_row
+            piece.col = new_col
+
+    def is_valid_move(self, current_row, current_col, new_row, new_col):
+    
+        # Return True if the move is valid, otherwise return False
+        return True  
