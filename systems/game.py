d1447ac39caf884e4044728ae7cef89aa9fafc92
Jakob Kirby <95430949+thatguy988@users.noreply.github.com>
2023-05-17 14:59:53 -0500

    Update game.py

--- systems/game.py
+++ systems/game.py
@@ -1,14 +1,17 @@
 import pygame
 import sys
 from components.board import Board
-WIDTH,HEIGHT=800,800
-rows,cols=8,8
+
+WIDTH, HEIGHT = 800, 800
+rows, cols = 8, 8
+
 class Game:
     def __init__(self):
-        square_size = WIDTH//cols
-        self.board = Board(rows,cols,square_size)
+        square_size = WIDTH // cols
+        self.board = Board(rows, cols, square_size)
         self.board.create_pieces()
-        self.dragging_piece = None
+        self.selected_piece = None
+        self.dragging = False
 
     def run(self):
         pygame.init()
@@ -23,23 +26,24 @@ class Game:
                     running = False
                     pygame.quit()
                     sys.exit()
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if event.button == 1:  # Left mouse button
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:  # Left mouse button pressed
                         mouse_pos = pygame.mouse.get_pos()
                         clicked_row = mouse_pos[1] // self.board.square_size
                         clicked_col = mouse_pos[0] // self.board.square_size
-                        self.dragging_piece = self.board.grid[clicked_row][clicked_col]
+                        self.selected_piece = self.board.grid[clicked_row][clicked_col]
+                        if self.selected_piece is not None:
+                            self.dragging = True
 
-                if event.type == pygame.MOUSEBUTTONUP:
-                    if event.button == 1:  # Left mouse button
-                        self.dragging_piece = None
-
-                if event.type == pygame.MOUSEMOTION:
-                    if self.dragging_piece is not None:
-                        mouse_pos = pygame.mouse.get_pos()
-                        self.dragging_piece.row = mouse_pos[1] // self.board.square_size
-                        self.dragging_piece.col = mouse_pos[0] // self.board.square_size
-
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    if event.button == 1:  # Left mouse button released
+                        if self.dragging and self.selected_piece is not None:
+                            mouse_pos = pygame.mouse.get_pos()
+                            new_row = mouse_pos[1] // self.board.square_size
+                            new_col = mouse_pos[0] // self.board.square_size
+                            self.board.move_piece(self.selected_piece, new_row, new_col)
+                        self.selected_piece = None
+                        self.dragging = False
 
             self.board.draw(screen)
 
